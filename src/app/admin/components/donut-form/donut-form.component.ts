0ce209e2dd5e29befb55889f05bd3475a346ac94
Maycon Jardim <mayconsmill@gmail.com>
2022-11-20 21:05:02 -0300

    donut-form validtion, event emiter

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -1,42 +1,101 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Donut } from '../../models/donut.model';
 
 @Component({
   selector: 'donut-form',
   template: `
-    <form class="donut-form" #form="ngForm">
+    <form class="donut-form" (ngSubmit)="handleSubmit(form)" #form="ngForm">
       <label>
         <span>Nome</span>
-        <input type="text" name="name" class="input" ngModel />
+        <input
+          type="text"
+          name="name"
+          class="input"
+          required
+          minlength="5"
+          [ngModel]="donut.name"
+          [ngModelOptions]="{ updateOn: 'submit' }"
+          #name="ngModel"
+        />
       </label>
 
+      <ng-container *ngIf="name.invalid && name.touched">
+        <div class="donut-form-error" *ngIf="name.errors?.required">
+          Nome é requerido.
+        </div>
+        <div class="donut-form-error" *ngIf="name.errors?.minlength">
+          Comprimento mínimo do nome são 5 letras
+        </div>
+      </ng-container>
+
       <label>
-        <span>Icon</span>
-        <select name="icon" class="input input--select" ngModel>
+        <span>Icone</span>
+        <select
+          name="icon"
+          class="input input--select"
+          required
+          [ngModel]="donut.icon"
+          #icon="ngModel"
+        >
           <option *ngFor="let icon of icons" [ngValue]="icon">
             {{ icon }}
           </option>
         </select>
+
+        <ng-container *ngIf="icon.invalid && icon.touched">
+          <div class="donut-form-error" *ngIf="icon.errors?.required">
+            Icone é requerido.
+          </div>
+        </ng-container>
       </label>
 
       <label>
         <span>Preço</span>
-        <input type="number" name="price" class="input" ngModel />
+        <input
+          type="number"
+          name="price"
+          class="input"
+          required
+          [ngModel]="donut.price"
+          #price="ngModel"
+        />
+        <ng-container *ngIf="price.invalid && price.touched">
+          <div class="donut-form-error" *ngIf="price.errors?.required">
+            Preço é requerido.
+          </div>
+        </ng-container>
       </label>
 
       <div class="donut-form-radios">
         <p class="donut-form-radios-label">Promo:</p>
         <label>
-          <input type="radio" name="promo" [value]="undefined" ngModel />
+          <input
+            type="radio"
+            name="promo"
+            [value]="undefined"
+            [ngModel]="donut.promo"
+          />
           <span>Nenhuma</span>
         </label>
 
         <label>
-          <input type="radio" name="promo" value="novo" ngModel />
+          <input
+            type="radio"
+            name="promo"
+            value="novo"
+            [ngModel]="donut.promo"
+          />
           <span>Novo</span>
         </label>
 
         <label>
-          <input type="radio" name="promo" value="limitado" ngModel />
+          <input
+            type="radio"
+            name="promo"
+            value="limitado"
+            [ngModel]="donut.promo"
+          />
           <span>Limitado</span>
         </label>
       </div>
@@ -46,10 +105,27 @@ import { Component, OnInit } from '@angular/core';
         <textarea
           name="description"
           class="input input--textarea"
-          ngModel
+          required
+          [ngModel]="donut.description"
+          #description="ngModel"
         ></textarea>
+        <ng-container *ngIf="description.invalid && description.touched">
+          <div class="donut-form-error" *ngIf="description.errors?.required">
+            Descrição é requerido.
+          </div>
+        </ng-container>
       </label>
 
+      <button type="submit" class="btn btn--green">Criar</button>
+      <button type="button" class="btn btn--grey" (click)="form.resetForm()">
+        Limpar
+      </button>
+
+      <div class="donut-form-working" *ngIf="form.valid && form.submitted">
+        Working...
+      </div>
+
+      <pre>{{ donut | json }}</pre>
       <pre>{{ form.value | json }}</pre>
     </form>
   `,
@@ -73,11 +149,25 @@ import { Component, OnInit } from '@angular/core';
             }
           }
         }
+        &-working {
+          font-size: 12px;
+          font-style: italic;
+          margin: 10px;
+          margin: 10px 0;
+        }
+
+        &-error {
+          font-size: 12px;
+          color: #e66262;
+        }
       }
     `,
   ],
 })
-export class DonutFormComponent implements OnInit {
+export class DonutFormComponent {
+  @Input() donut!: Donut;
+  @Output() create = new EventEmitter<Donut>();
+
   icons: string[] = [
     'caramel-swirl',
     'glazed-fudge',
@@ -89,5 +179,11 @@ export class DonutFormComponent implements OnInit {
   ];
   constructor() {}
 
-  ngOnInit(): void {}
+  handleSubmit(form: NgForm) {
+    if (form.valid) {
+      this.create.emit(form.value);
+    } else {
+      form.form.markAllAsTouched();
+    }
+  }
 }
--- src/app/admin/containers/donut-single/donut-single.component.ts
+++ src/app/admin/containers/donut-single/donut-single.component.ts
@@ -1,16 +1,31 @@
+import { Donut } from './../../models/donut.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'donut-single',
   template: `
     <div>
-      <donut-form></donut-form>
+      <donut-form [donut]="donut" (create)="onCreate($event)"></donut-form>
     </div>
   `,
   styles: [],
 })
 export class DonutSingleComponent implements OnInit {
+  donut!: Donut;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.donut = {
+      id: 'y8z0s',
+      name: 'Chocolate',
+      icon: 'just-chocolate',
+      price: 19.99,
+      promo: 'limitado',
+      description: 'Apenas para os viciados em chocolate.',
+    };
+  }
+
+  onCreate(donut: Donut) {
+    console.log('onCreate', donut);
+  }
 }
